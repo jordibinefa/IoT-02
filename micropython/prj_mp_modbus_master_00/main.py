bd601c7923a3959f34c5399190136e8bd7092854
Jordi Binefa <40146788+jordibinefa@users.noreply.github.com>
2022-08-30 12:57:38 +0200

    Update main.py

--- micropython/prj_mp_modbus_master_00/main.py
+++ micropython/prj_mp_modbus_master_00/main.py
@@ -1,3 +1,10 @@
+'''
+Based on: https://github.com/eydam-prototyping/mp_modbus
+
+https://github.com/jordibinefa/IoT-02/tree/master/micropython/prj_mp_modbus_master_00
+
+20220830
+'''
 import utime
 utime.sleep(4)
 # import range
